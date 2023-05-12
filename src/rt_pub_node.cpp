400540264b842ad0b6b4255102a49ae4aae2b4df
RomanHarmady <roman.harmady98@gmail.com>
2023-05-12 11:21:32 +0200

    first commit

--- src/rt_pub_node.cpp
+++ src/rt_pub_node.cpp
@@ -3,19 +3,27 @@
 #include <iostream>
 #include <chrono>
 #include <memory>
+#include <cstdint>
 
 #include "rttest/rttest.h"
 #include "rclcpp/rclcpp.hpp"
 
-#include "std_msgs/msg/int32.hpp"
 #include "std_msgs/msg/int16.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/int64.hpp"
+
+
+
 
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
 uint8_t P_GAIN = 30;
-uint16_t pos;
+uint32_t pos;
+auto message = std_msgs::msg::Int16();
+uint16_t mess;
+
 
 
 class RTPublisher : public rclcpp::Node
@@ -24,8 +32,8 @@ public:
 
     RTPublisher(): Node("rt_pub_node"), count(0)            
     {
-        publisher_ = this->create_publisher<std_msgs::msg::Int16>("motor_subscriber", 10);
-        subscriber_ = this->create_subscription<std_msgs::msg::Int16>("encoder_publisher", 10, std::bind(&RTPublisher::callback, this, std::placeholders::_1));
+        publisher_ = this->create_publisher<std_msgs::msg::Int16>("log_publisher", 10);
+        subscriber_ = this->create_subscription<std_msgs::msg::Int16>("encoder_subscriber", 10, std::bind(&RTPublisher::callback, this, std::placeholders::_1));
 
         timer_ = this->create_wall_timer(1ms, std::bind(&RTPublisher::timer_callback, this));
     }
@@ -43,16 +51,19 @@ private:
 
 
     void timer_callback() {                     
-        auto start_time = std::chrono::steady_clock::now();
-        auto message = std_msgs::msg::Int16();
+        auto start_time = std::chrono::high_resolution_clock::now();
         message.data = 0;
         if(pos >= 30){
             message.data = pos * P_GAIN;
         }
-        publisher_->publish(message);
-        auto end_time = std::chrono::steady_clock::now();
+
+        auto end_time = std::chrono::high_resolution_clock::now();
         auto execution_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-        RCLCPP_INFO(this->get_logger(), "Execution time: %ld microseconds", execution_time.count());
+       // RCLCPP_INFO(get_logger(), "miliseconds: %ld", execution_time);
+        message.data = execution_time.count();
+        publisher_->publish(message);
+        
+        
         
     }   
 };
